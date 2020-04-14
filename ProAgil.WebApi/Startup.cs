258f3c8faf6afda3825234f7bc1888527b45c1e7
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2020-04-14 10:03:41 -0300

    Adicionando nav-bar e filtro two way data binding

--- ProAgil.WebApi/Startup.cs
+++ ProAgil.WebApi/Startup.cs
@@ -48,6 +48,7 @@ namespace ProAgil.WebApi
 
             //app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
